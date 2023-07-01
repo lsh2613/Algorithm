0af41f9d185915f2f643b73c47f26b78453e7adc
lsh2613 <lsh2613@naver.com>
2023-07-01 23:05:01 +0900

    정렬 기출

--- src/test.java
+++ src/test.java
@@ -1,25 +1,8 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class test {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        String[] input = br.readLine().split(" ");
-        int[] ints = new int[N];
-        int[] sortedInts = new int[N];
-
-        for (int i = 0; i < N; i++)
-            sortedInts[i] = ints[i]= Integer.parseInt(input[i]);
-
-        Arrays.sort(sortedInts);
-        for (int i = 0; i < N; i++) {
-            int idx = Arrays.binarySearch(sortedInts, ints[i]);
-            System.out.print(idx+" ");
-        }
 
     }
 }
