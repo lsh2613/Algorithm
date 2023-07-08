7549d6205743aa9f760038ba7257788b64dbbc46
lsh2613 <lsh2613@naver.com>
2023-07-08 23:15:50 +0900

    swExpert

---
+++ src/Solution.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class Solution
+{
+    public static long lcm(long a, long b) {
+        int gcd_value = gcd((int)a, (int)b);
+
+        if (gcd_value == 0) return 0; // 인수가 둘다 0일 때의 에러 처리
+
+        return Math.abs( (a * b) / gcd_value );
+    }
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return Math.abs(a);
+    }
+    public static void main(String args[]) throws Exception
+    {
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            String str1 = sc.next();
+            String str2 = sc.next();
+            int len1 = str1.length();
+            int len2 = str2.length();
+            long lcm = lcm(len1, len2);
+            String result1 = "";
+            String result2 = "";
+            for (int i = 0; lcm != i * len1; i++) {
+                result1 += str1;
+            }
+            for (int i = 0; lcm != i * len2; i++) {
+                result2 += str2;
+            }
+            System.out.printf("#%d ",test_case);
+            System.out.println(result1.equals(result2) ? "yes" : "no");
+        }
+    }
+}
--- src/test.java
+++ src/test.java
@@ -1,8 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        int[] cs = new int[]{1, 2};
-        System.out.println(String.valueOf(cs));
-//        System.out.println(cs.toString());
-//        System.out.println(Integer.parseInt(cs.toString()));
+        String str = "ABCD";
+        System.out.println();
     }
 }
