0116e0b4ffd0fac029f4068dde1b997eacf10ba3
lsh2613 <lsh2613@naver.com>
2023-08-09 22:33:13 +0900

    8/9

--- src/sol1.java
+++ src/sol1.java
@@ -1,43 +1,95 @@
-class sol1 {
-    static boolean[] vs;
-    static String[] words;
-    static int answer;
-    public int solution(String begin, String target, String[] words) {
-        this.words=words;
-        vs = new boolean[words.length];
+import java.util.*;
 
-        dfs(begin, target, 1);
+class Genre{
+    String name;
+    int cnt;
+    int idx;
+
+    int getCnt(){
+        return cnt;
+    }
+
+    Genre(String n, int c){
+        name=n;
+        cnt=c;
+    }
+    Genre(int i, int c){
+        idx=i;
+        cnt=c;
+    }
+}
+
+class sol1 {
+    public int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+        StringBuilder sb = new StringBuilder();
+
+        // 장르별 재생횟수 합계
+        Map<String, Integer> genreTotal = new HashMap<>();
+        // 장르별 각 노래들의 정보
+        Map<String, ArrayList<Genre>> genre_songs = new HashMap<>();
+
+        // map 초기화
+        for(int i=0; i<genres.length; i++){
+            String genre = genres[i];
+            int playCnt = plays[i];
+
+            if(genreTotal.containsKey(genre))
+                genreTotal.put(genre, genreTotal.get(genre)+playCnt);
+            else
+                genreTotal.put(genre, playCnt);
+
+            if(genre_songs.containsKey(genre)){
+                ArrayList<Genre> songs= genre_songs.get(genre);
+                songs.add(new Genre(i, playCnt));
+                genre_songs.put(genre, songs);
+            }
+            else{
+                ArrayList<Genre> songs= new ArrayList<>();
+                songs.add(new Genre(i, playCnt));
+                genre_songs.put(genre, songs);
+            }
+        }
+
+        ArrayList<Genre> genreList = new ArrayList<>();
+        for(String genre : genreTotal.keySet()){
+            genreList.add(new Genre(genre, genreTotal.get(genre)));
+        }
+
+        // 내림차순 정렬
+        genreList.sort(Comparator.comparingInt(Genre::getCnt).reversed());
+
+        // 재생 횟수가 많은 장르부터 탐색
+        for (Genre genre : genreList) {
+            String name = genre.name;
+            ArrayList<Genre> songs = genre_songs.get(name);
+            songs.sort((a,b)->{
+                if(a.cnt==b.cnt)
+                    return a.idx-b.idx;
+                return b.cnt-a.cnt;
+            });
+
+            // 최대2개
+            int len=0;
+            if(songs.size()%2==1){
+                len= songs.size()-1;
+            }
+            else len=songs.size();
+
+            for (int i = 0; i < len; i++) {
+                sb.append(songs.get(i).idx+" ");
+            }
+
+        }
+
+        answer = Arrays.stream(sb.toString().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
         return answer;
     }
 
-    static void dfs(String now, String target, int cnt){
-        if(now.equals(target)){
-            answer=cnt;
-            return;
-        }
-
-        for(int i=0; i<words.length; i++){
-            if(vs[i]||!canChange(now, words[i])) continue;
-
-            vs[i]=true;
-            dfs(words[i], target, cnt++);
-            vs[i]=false;
-        }
-    }
-
-    static boolean canChange(String s1, String s2){
-        int cnt=0;
-        for(int i=0;i<s1.length();i++){
-            if(s1.charAt(i)==s2.charAt(i))
-                cnt++;
-        }
-        return cnt==s1.length()-1?true:false;
-    }
-
-    public static void main(String[] args) {
-        sol1 solution = new sol1();
-        String[] words = new String[]{"hot", "dot", "dog", "lot", "log", "cog"};
-        int result = solution.solution("hit", "cog", words);
-        System.out.println("XOR 결과: " + result);
-    }
 }
+
+
+
