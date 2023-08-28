9347e19814ccd8d5cffb7af0b9de64d6c399310f
lsh2613 <lsh2613@naver.com>
2023-08-28 21:46:42 +0900

    8/28

---
+++ src/백준/b10816.java
@@ -0,0 +1,32 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class b10816 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<Integer, Integer> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < M; i++) {
+            int key = Integer.parseInt(st.nextToken());
+
+            sb.append(map.getOrDefault(key, 0)).append(' ');
+        }
+
+        System.out.println(sb);
+    }
+}
