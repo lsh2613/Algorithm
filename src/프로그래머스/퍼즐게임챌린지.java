a11a3326e73d07ef673be72e22125c5c9160dda9
LSH <lsh2613@naver.com>
2024-12-19 16:19:21 +0900

    프로그래머스 퍼즐게임챌린지 설명 추가

--- src/프로그래머스/퍼즐게임챌린지.java
+++ src/프로그래머스/퍼즐게임챌린지.java
@@ -2,6 +2,19 @@ package 프로그래머스;
 
 import java.util.Arrays;
 
+/**
+ * [시간초과 발생하는 풀이]
+ * - level=1부터 순차적으로 time을 구하고, time<=limit이라면 return
+ * - diff의 최대 길이 300,000
+ * - level은 diffs의 최댓값만큼 가질 수 있으므로 최대 300,000
+ * - 각 level마다 diff를 통한 time 구하기 = 300,000 * 300,000 = 9e^10(90억)
+ *
+ * [개선 방법 - 이분 탐색]
+ * - 어차피 level은 diff의 최댓값까지 가질 수 있으므로 1<=level<=300,000
+ * - 즉 level을 순차적으로 증가시키는 것이 아니라 '이분 탐색'을 통해 범위를 1/2씩 줄인다
+ * - 따라서 이분 탐색의 시간 복잡도 O(log N)을 적용했을 때, level의 최대 횟수는 1<=level<=약 20
+ * - 전체 시간 복잡도 = 300,000 * 20 = 6,000,000
+ */
 public class 퍼즐게임챌린지 {
     public int solution(int[] diffs, int[] times, long limit) {
         int maxValue = Arrays.stream(diffs).max().getAsInt();
